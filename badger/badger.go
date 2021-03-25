3b7ba5aef7946650ca4dc67cf47be3699f6cda7d
ucwong <ucwong@126.com>
2021-03-25 20:39:58 +0800

    range search opt by seek

--- badger/badger.go
+++ badger/badger.go
@@ -108,20 +108,15 @@ func (b *Badger) SetTTL(k, v []byte, expire time.Duration) (err error) {
 func (b *Badger) Range(start, limit []byte) (res [][]byte) {
 	b.engine.View(func(txn *badger.Txn) error {
 		opts := badger.DefaultIteratorOptions
-		opts.PrefetchSize = 10
 		it := txn.NewIterator(opts)
 		defer it.Close()
-		for it.Rewind(); it.Valid(); it.Next() {
-			item := it.Item()
-			//fmt.Printf("%s, %s, %s, %v, %v\n", string(start), string(limit), string(item.Key()), bytes.Compare(start, item.Key()), bytes.Compare(limit, item.Key()))
-			if bytes.Compare(start, item.Key()) <= 0 {
-				if bytes.Compare(limit, item.Key()) > 0 {
-					if val, err := item.ValueCopy(nil); err == nil {
-						res = append(res, val)
-					}
-				} else {
-					break
+		for it.Seek(start); it.Valid() && bytes.Compare(start, it.Item().Key()) <= 0; it.Next() {
+			if bytes.Compare(limit, it.Item().Key()) > 0 {
+				if val, err := it.Item().ValueCopy(nil); err == nil {
+					res = append(res, val)
 				}
+			} else {
+				break
 			}
 		}
 		return nil
