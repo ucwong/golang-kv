9bd7e7e618fc7afa905ed988ae31560537bb08ed
ucwong <ucwong@126.com>
2021-03-30 11:38:07 +0800

    for each scan

--- bolt/bolt.go
+++ bolt/bolt.go
@@ -107,12 +107,19 @@ func (b *Bolt) Prefix(prefix []byte) (res [][]byte) {
 func (b *Bolt) Suffix(suffix []byte) (res [][]byte) {
 	b.engine.View(func(tx *bolt.Tx) error {
 		if buk := tx.Bucket([]byte(GLOBAL)); buk != nil {
-			c := buk.Cursor()
+			/*c := buk.Cursor()
 			for k, v := c.First(); k != nil; k, v = c.Next() {
 				if bytes.HasSuffix(k, suffix) {
 					res = append(res, common.SafeCopy(nil, v))
 				}
-			}
+			}*/
+
+			buk.ForEach(func(k, v []byte) error {
+				if bytes.HasSuffix(k, suffix) {
+					res = append(res, common.SafeCopy(nil, v))
+				}
+				return nil
+			})
 		}
 
 		return nil
